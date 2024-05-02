31746b0d9e866b2da3c48f071e804f0da0d985ff
Lutiero <3720@ucdb.br>
2024-05-02 07:44:10 -0400

    Update timer controls icons and actions

--- js/FocusTimer/actions.js
+++ js/FocusTimer/actions.js
@@ -1,3 +1,18 @@
+import state from "./state.js"
+
 export function toggleRunning() {
-  console.log("toggleRunning function called");
+  const playButton = document.querySelector(".ph-play-circle")
+  const stopButton = document.querySelector(".ph-stop-circle")
+
+  state.isRunning = document.documentElement.classList.toggle("running")
+  playButton.toggleAttribute("hidden")
+  stopButton.toggleAttribute("hidden")
+}
+
+export function plusFiveSeconds() {
+  console.log("plusFiveSeconds function called")
+}
+
+export function minusFiveSeconds() {
+  console.log("minusFiveSeconds function called")
 }
